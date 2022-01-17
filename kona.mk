a4bf526f28bf5b1cd08c5e130cbf65d27018109f
RealSchnedi <schnedi3@gmail.com>
2022-01-17 16:13:38 +0100

    sm8250-common: Import Lights HAL
    * From msm8953-common

--- kona.mk
+++ kona.mk
@@ -310,7 +310,7 @@ PRODUCT_PACKAGES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    android.hardware.light-service.xiaomi
+    android.hardware.lights-service.xiaomi_kona
 
 # LiveDisplay
 PRODUCT_PACKAGES += \
